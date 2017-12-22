42cf16e86c11fb2cd661fccec2a4144dfbbcf579
Kian Moretz <kian@orioncomputing.net>
2017-12-22 18:06:15 -0500

    Build admin authorization code

---
+++ src/functions/authorizeAdmin.js
@@ -0,0 +1,37 @@
+/* 
+   Copyright (c) 2017 Orion Computing LLC.
+   Developed by Kian Moretz.
+   This code is licensed under the terms of the MIT license.
+   See the LICENSE file in the root of this project directory for license terms.
+*/
+(function() {
+  "use strict";
+
+  // Import user model
+  const User = require("../models/user");
+
+  // Import config
+  const config = require("../config");
+
+  // Import jwt
+  const jwt = require("jsonwebtoken");
+
+  // Create function
+  const authorizeAdmin = async ctx => {
+    // Grab token from header
+    const token = ctx.request.headers["Authorization"].split(" ")[1];
+    if (!token) return false;
+    // Verify JWT
+    let decoded;
+    try {
+      decoded = jwt.verify(token, config.secret);
+    } catch (error) {
+      return false;
+    }
+    // Find user and check for admin privileges
+    const user = await User.findOne({ email: decoded.email });
+    if (user.isAdmin) return true;
+  };
+
+  module.exports = authorizeAdmin;
+})();
